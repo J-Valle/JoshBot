38247799ea2ae2647bf57aec922995e5c098cc5e
J-Valle <josuedavid.valle94@gmail.com>
2022-03-14 11:49:33 +0100

    HTTP4S server-side implementation

--- build.sbt
+++ build.sbt
@@ -4,4 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.13.8"
 
-libraryDependencies += "com.github.pureconfig" %% "pureconfig" % "0.17.1"
+val http4sVersion = "0.23.10"
+
+libraryDependencies ++= Seq(
+  "com.github.pureconfig" %% "pureconfig" % "0.17.1",
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-client" % http4sVersion
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,23 @@
+import cats.effect.IOApp
 import config.Config
 import pureconfig._
 import pureconfig.generic.auto._
+import cats.effect._
+import org.http4s.blaze.server.BlazeServerBuilder
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world")
-    ConfigSource.default.loadOrThrow[Config]
-  }
+import scala.concurrent.ExecutionContext.global
+object Main extends IOApp {
+//  def main(args: Array[String]): Unit = {
+//    println("Hello world")
+//    ConfigSource.default.loadOrThrow[Config]
+//  }
+  val routes = new HttpRoutes
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(routes.helloWorldService.orNotFound)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
